9ed9002aca51f0d311995d2148c47ba28cbc3b88
Omonge Calvince <calvo-linus@localhost.localdomain>
2019-06-10 06:46:39 +0300

    user directive spec version

---
+++ src/app/user-det.directive.spec.ts
@@ -0,0 +1,8 @@
+import { UserDetDirective } from './user-det.directive';
+
+describe('UserDetDirective', () => {
+  it('should create an instance', () => {
+    const directive = new UserDetDirective();
+    expect(directive).toBeTruthy();
+  });
+});
