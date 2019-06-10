aca407e5a6642b8cf7cd75f6819751681790dd94
Omonge Calvince <calvo-linus@localhost.localdomain>
2019-06-10 10:00:35 +0300

    Auto-generated commit

--- src/app/classes/repo.ts
+++
@@ -1,7 +0,0 @@
-export class Repo {
-    constructor(
-        public name: string,
-        public html_url: string,
-        public description: string
-    ){}
-}
--- src/app/user-det.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserDetDirective } from './user-det.directive';
-
-describe('UserDetDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UserDetDirective();
-    expect(directive).toBeTruthy();
-  });
-});
